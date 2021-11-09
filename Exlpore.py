663a700f782a788cfd99caa3a47adb9c23384388
olangacheryl <91678812+olangacheryl@users.noreply.github.com>
2021-11-09 10:33:48 -0500

    Update Exlpore.py

--- Exlpore.py
+++ Exlpore.py
@@ -18,6 +18,21 @@ def explore():
     return c1 + c2 + c3
 
 
+import random
+
+h=[9,7,11]
+d=[3,5,7]
+days= 10000
+m1=days//2
+def explore():
+        happy=0
+        for num in range(m1):
+                happy=happy+random.normalvariate(h[0],d[0])
+        for num in range(m1):
+                happy=happy+random.normalvariate(h[1],d[1])
+        for num in range(m1):
+                happy=happy+random.normalvariate(h[2],d[2])
+        return happy
 
 
 
