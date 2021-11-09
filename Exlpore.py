29ae326decd17cf57832623fd753e212a51a24b0
Labeeb Alam <91550959+Labeeb-Alam@users.noreply.github.com>
2021-11-09 10:37:02 -0500

    Update Exlpore.py

--- Exlpore.py
+++ Exlpore.py
@@ -1,38 +1,16 @@
 import random
 
-c1 = random.normalvariate(9, 3)
-c2 = random.normalvariate(7, 5)
-c3 = random.normalvariate(11, 7)
-
-# Goes to every cafeteria for 100 days each
-def explore():
-    c1 = 0
-    c2 = 0
-    c3 = 0
-    for x in range(100):
-        c1 += random.normalvariate(9, 3)
-    for x in range(100):
-        c2 += random.normalvariate(7, 5)
-    for x in range(100):
-        c3 += random.normalvariate(11, 7)
-    return c1 + c2 + c3
-
-
-import random
-
-h=[9,7,11]
-d=[3,5,7]
-days= 300
+h = [9,7,11]
+d = [3,5,7]
+days = 300
 m1=days//3
+
 def explore():
         happy=0
         for num in range(m1):
-                happy=happy+random.normalvariate(h[0],d[0])
+                happy = happy + random.normalvariate(h[0],d[0])
         for num in range(m1):
-                happy=happy+random.normalvariate(h[1],d[1])
+                happy = happy + random.normalvariate(h[1],d[1])
         for num in range(m1):
-                happy=happy+random.normalvariate(h[2],d[2])
+                happy = happy + random.normalvariate(h[2],d[2])
         return happy
-
-
-
